d16907ced774c90df12b67d3efb2e3963b1e787a
paulo <henrique_santiago@outlook.com.br>
2025-02-09 15:11:54 -0300

    - att

--- src/components/actions/Dialog/components/DialogWithActions/index.tsx
+++ src/components/actions/Dialog/components/DialogWithActions/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "../../../../../components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -8,7 +8,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
+} from "../../../../../components/ui/dialog";
 
 type DialogWithActionsProps = React.PropsWithChildren & {
   title?: string;
