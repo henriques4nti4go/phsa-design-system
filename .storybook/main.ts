356c5fbb4e1a114cb40e11ec7824d8d69207e0a9
paulo <henrique_santiago@outlook.com.br>
2026-01-22 21:30:59 -0300

    att

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
+const basePath = process.env.STORYBOOK_BASE_PATH || '';
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -12,5 +14,6 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ["../public"],
+  ...(basePath ? { base: basePath } : {}),
 };
 export default config;
